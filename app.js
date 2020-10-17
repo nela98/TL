84980a97a0428884023cd3b5e57b1275a4a5320b
nela98 <daniela.martinez7@udea.edu.co>
2020-10-17 18:00:10 -0500

    Updated

---
+++ app.js
@@ -0,0 +1,116 @@
+function abrir(event){
+    let archivo = event.target.files[0];
+    if(archivo){
+        let reader = new FileReader();
+        reader.onload = function(e){
+            let contenido = e.target.result;
+            var lines = contenido.split('\n');
+            function convertirLiga(lines);
+            document.getElementById('contenido').innerText = contenido;
+            document.getElementById('resultado').innerText = "Todo malo :D";
+        }
+        reader.readAsText(archivo);
+    }else{
+        document.getElementById("mensaje").innerText = "No ha seleccionado archivo"
+    } return lines
+}
+
+window.addEventListener("load",()=>{
+    document.getElementById("loader").addEventListener("change",abrir)    
+})
+
+//Convertir lineas en listas.
+function convertirLiga(lines){
+  var arreglo =  a;
+}
+
+/********************************************************************************************************* */
+// Creación de listas
+
+function Node (clase, value) {
+    this.clase = clase
+    this.value = value
+    this.next = null
+  }
+  
+  function LinkedList() {
+    this.head = null
+  }
+  
+  LinkedList.prototype.append = function(clase,value, current = this.head){
+    if(this.head === null){
+      return this.head = new Node(clase,value)
+    }
+    if(current.next === null){
+      return current.next = new Node(clase,value)
+    }
+    this.append(clase,value, current.next)
+  }
+  
+  LinkedList.prototype.prepend = function(clase,value){
+    if(this.head === null){
+      return this.head = new Node(clase,value)
+    }
+    let newNode = new Node(clase,value)
+    newNode.next = this.head
+    this.head = newNode
+  }
+  
+  LinkedList.prototype.removeNode = function (value, current = this.head) {
+    if(this.head === null){ // no head
+      return false
+    }
+  
+    if (this.head.value === value){
+      return this.head = this.head.next
+    }
+  
+    if(current.next !== null){
+      if(current.next.value === value){
+        return current.next = current.next.next
+      }
+      this.removeNode(value, current.next)
+    }
+    return false // no match found
+  }
+  
+  LinkedList.prototype.findNode = function (value, current = this.head){
+    if(this.head === null) {
+      return false
+    }
+  
+    if (current !== null) {
+      if (current.value === value){
+        return true
+      } else {
+        return this.findNode(value, current.next)
+      }
+    }
+    return false
+  }
+  
+  LinkedList.prototype.peekNode = function (value) {
+    if(this.head === null) {
+      return false
+    }
+    return this.head
+  }
+  
+  LinkedList.prototype.length = function (current = this.head, acum = 1) {
+    if(this.head === null){
+      return 0
+    }
+    if (current.next !== null){
+      return this.length(current.next, acum = acum + 1)
+    }
+    return acum
+  }
+  //const linkedlist = new LinkedList();
+ // linkedlist.append('int',12);
+ // console.log(linkedlist);
+
+ /* linkedlist.append(27);
+  linkedlist.append(19);
+*/
+ 
+  /********************************************************************************************************************* */
